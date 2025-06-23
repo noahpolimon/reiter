2527f73352d5a41471aff80889dad647a75b33b1
noahpolimon <polimonnoah@gmail.com>
2025-06-24 00:35:12 +0800

    fix error names in expectImplIter()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -35,6 +35,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Overflow error handling in `Chain`
 - Fix doc-comments in `Iter`
 - `AsIter` type checking
+- Error names in `expectImplIter()`
 
 ## [0.5.0] - 2025-06-02
 
--- src/meta_extra.zig
+++ src/meta_extra.zig
@@ -15,7 +15,11 @@ pub inline fn isMarked(comptime T: type, comptime name: []const u8) bool {
     return false;
 }
 
-pub inline fn checkIterConstraints(comptime I: type, comptime name: []const u8, comptime Item: type) bool {
+pub inline fn checkIterConstraints(
+    comptime I: type,
+    comptime name: []const u8,
+    comptime Item: type,
+) bool {
     if (!@hasField(I, "wrapped")) return false;
     const Wrapped = @FieldType(I, "wrapped");
     return comptime I == Iter(Wrapped) and Wrapped.Item == Item and isMarked(Wrapped, name);
@@ -24,15 +28,15 @@ pub inline fn checkIterConstraints(comptime I: type, comptime name: []const u8, 
 pub inline fn expectImplIter(comptime T: type) !void {
     // check for Item
     if (!@hasDecl(T, "Item"))
-        return error.NoPubItemDecl;
+        return error.NoPublicItemDecl;
 
     if (@TypeOf(T.Item) != type)
-        return error.ItemDeclWrongType;
+        return error.WrongItemDeclType;
 
     // check for next()
     if (!meta.hasMethod(T, "next"))
-        return error.NoPubNextMethod;
+        return error.NoPublicNextMethod;
 
     if (@TypeOf(T.next) != fn (*T) ?T.Item)
-        return error.NextMethodWrongSignature;
+        return error.WrongNextMethodType;
 }
